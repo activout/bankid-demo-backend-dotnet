9be83143fda0ac04709bec76bf70ef323c2599fe
David Eriksson <david@activout.se>
2023-12-17 13:27:12 +0100

    Generated code

---
+++ BankIdDemo.Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BankIdDemo.Backend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
